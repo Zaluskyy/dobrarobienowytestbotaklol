e087a7a8caf1634fd96edf3bb47ef5721af5df0c
Your-Full-Name <kryzal77@gmail.com>
2021-07-18 22:00:46 +0200

    dd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import React, { useRef, useEffect } from 'react';
+
+
 import './App.css';
 
 function App() {
+  const canvas = useRef(null)
+  const requestRef = React.useRef()
+
+  class Magic{
+    constructor(x, y){
+      this.x = x
+      this.y = y
+    }
+    draw(){
+      const c = canvas.current.getContext("2d");
+      c.arc(this.x, this.y, 50, 0, Math.PI*2, false)
+      c.stroke()
+    }
+  }
+
+  const magic = new Magic(200, 200)
+
+  useEffect(() => {
+   animate()
+  });
+
+  const animate = ()=>{
+    const c = canvas.current.getContext("2d");
+    requestRef.current = requestAnimationFrame(animate);
+    c.clearRect(0, 0, window.innerWidth, window.innerHeight)
+    magic.draw()
+  }
+
   return (
     <div className="App">
-      JD
+      <canvas ref={canvas} width={500} height={500} style={{ marginTop:"150px", backgroundColor: "white"}} ></canvas>
     </div>
   );
 }
