d17ec1bc9965cbb8abf29cd6f34a915102f20b8e
Your-Full-Name <kryzal77@gmail.com>
2021-07-18 23:32:13 +0200

    ii

--- src/App.js
+++ src/App.js
@@ -7,6 +7,21 @@ function App() {
   const canvas = useRef(null)
   const requestRef = React.useRef()
 
+  const randMinMax = (min, max, floor=false)=>{
+    if(!floor){
+    return Math.random()*(max-min)+min
+    }
+    return Math.floor(Math.random()*(max-min+1)+min)
+  }
+
+  console.log(randMinMax(2, 5, false))
+
+  const distance = (x1, y1, x2, y2)=>{
+    const distanceX = x1 - x2
+    const distanceY = y1 - y2
+    return Math.sqrt(Math.pow(distanceX, 2)+Math.pow(distanceY, 2))
+  }
+
   class Magic{
     constructor(x, y){
       this.x = x
@@ -14,14 +29,26 @@ function App() {
     }
     draw(){
       const c = canvas.current.getContext("2d");
+      c.beginPath()
       c.arc(this.x, this.y, 50, 0, Math.PI*2, false)
       c.stroke()
     }
   }
 
-  const magic = new Magic(200, 200)
+  const magic = []
+
+  const init =()=>{
+    for(let i = 0; i < 10; i++){
+      const x = randMinMax(0+50, canvas.current.width-50)
+      const y = randMinMax(0+50, canvas.current.height-50)
+      magic.push(new Magic(x, y))
+    }
+  }
+
+  // const magic = new Magic(200, 200)
 
   useEffect(() => {
+    init()
    animate()
   });
 
@@ -29,7 +56,10 @@ function App() {
     const c = canvas.current.getContext("2d");
     requestRef.current = requestAnimationFrame(animate);
     c.clearRect(0, 0, window.innerWidth, window.innerHeight)
-    magic.draw()
+    for(let i = 0; i < magic.length; i++){
+      magic[i].draw()
+    }
+    // magic.draw()
   }
 
   return (
