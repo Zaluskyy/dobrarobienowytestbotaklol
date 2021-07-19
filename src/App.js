4cbf8b4d989d77c4fbd8366dba48107f83528bb5
Your-Full-Name <kryzal77@gmail.com>
2021-07-20 01:18:02 +0200

    jkfd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 
 
 import './App.css';
+
+
+const randMinMax = (min, max, floor=false)=>{
+  if(!floor){
+  return Math.random()*(max-min)+min
+  }
+  return Math.floor(Math.random()*(max-min+1)+min)
+}
+
+const distance = (x1, y1, x2, y2)=>{
+  const distanceX = x1 - x2
+  const distanceY = y1 - y2
+  return Math.sqrt(Math.pow(distanceX, 2)+Math.pow(distanceY, 2))
+}
 
 function App() {
   const canvas = useRef(null)
   const requestRef = React.useRef()
 
-  const randMinMax = (min, max, floor=false)=>{
-    if(!floor){
-    return Math.random()*(max-min)+min
-    }
-    return Math.floor(Math.random()*(max-min+1)+min)
-  }
-
-  console.log(randMinMax(2, 5, false))
-
-  const distance = (x1, y1, x2, y2)=>{
-    const distanceX = x1 - x2
-    const distanceY = y1 - y2
-    return Math.sqrt(Math.pow(distanceX, 2)+Math.pow(distanceY, 2))
-  }
+  
 
   class Magic{
     constructor(x, y){
@@ -64,7 +65,7 @@ function App() {
 
   return (
     <div className="App">
-      <canvas ref={canvas} width={500} height={500} style={{ marginTop:"150px", backgroundColor: "white"}} ></canvas>
+      <canvas ref={canvas} width={500} height={500} style={{ marginTop:"150px", backgroundColor: "yellow"}} ></canvas>
     </div>
   );
 }
